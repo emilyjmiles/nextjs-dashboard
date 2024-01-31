f7db3a1535cbce73f61d2badb01a8bbdf9fe9ae2
Emily Miles <ms.emily.j.miles@gmail.com>
2024-01-31 18:48:08 -0500

    Add links for navigation

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -1,14 +1,18 @@
-'use client';
- 
 import {
   UserGroupIcon,
   HomeIcon,
-  InboxIcon,
+  DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
  
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon, },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
  
 export default function NavLinks() {
   const pathname = usePathname();
